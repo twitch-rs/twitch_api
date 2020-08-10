fddf8edcab123ef2d30809467ef8df8b47a2742f
Emil Gardström <emil.gardstrom@gmail.com>
2020-08-10 20:11:35 +0200

    add example for user token

---
+++ oauth2/examples/user_token.rs
@@ -0,0 +1,13 @@
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().unwrap();
+    let mut args = std::env::args().skip(1);
+    let token = twitch_oauth2::UserToken::from_existing(
+        std::env::var("TWITCH_TOKEN").ok().or_else(|| args.next())
+            .expect("Please set env: TWITCH_TOKEN or pass token as first argument"),
+            std::env::var("TWITCH_REFRESH_TOKEN").ok().or_else(|| args.next())
+    )
+    .await
+    .unwrap();
+    println!("{:?}", token);
+}
