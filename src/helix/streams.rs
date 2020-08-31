180f9ee8da791eff19ccfd9cf77538c0baf2165c
Emil Gardström <emil.gardstrom@gmail.com>
2020-08-31 22:31:59 +0200

    rename GetStreams -> Streams

--- src/helix/streams.rs
+++ src/helix/streams.rs
@@ -20,7 +20,7 @@
 //! # }
 //! ```
 #[doc(inline)]
-pub use get_streams::{GetStreams, GetStreamsRequest};
+pub use get_streams::{Streams, GetStreamsRequest};
 
 use crate::helix;
 use serde::{Deserialize, Serialize};
@@ -99,7 +99,7 @@ pub mod get_streams {
     /// [`get-streams`](https://dev.twitch.tv/docs/api/reference#get-streams)
     #[derive(PartialEq, Deserialize, Debug, Clone)]
     #[non_exhaustive]
-    pub struct GetStreams {
+    pub struct Streams {
         /// ID of the game being played on the stream.
         pub game_id: String,
         /// Stream ID.
@@ -126,7 +126,7 @@ pub mod get_streams {
     }
 
     impl helix::Request for GetStreamsRequest {
-        type Response = GetStreams;
+        type Response = Streams;
 
         const PATH: &'static str = "streams";
         const SCOPE: &'static [twitch_oauth2::Scope] = &[];
