57a87b913374a9d2ff80e018e896f4566ca9f991
Emil Gardström <emil.gardstrom@gmail.com>
2020-09-01 00:06:59 +0200

    implement get user follows

--- src/helix/users.rs
+++ src/helix/users.rs
@@ -21,6 +21,9 @@
 #[doc(inline)]
 pub use get_users::{GetUsersRequest, User};
 
+#[doc(inline)]
+pub use get_users_follows::{GetUsersFollowsRequest, UsersFollows};
+
 use crate::helix;
 use serde::{Deserialize, Serialize};
 use typed_builder::TypedBuilder;
@@ -83,4 +86,71 @@ pub mod get_users {
     }
 
     impl helix::RequestGet for GetUsersRequest {}
+}
+
+/// Gets information on follow relationships between two Twitch users.
+/// [`get-users-follows`](https://dev.twitch.tv/docs/api/reference#get-users-follows)
+pub mod get_users_follows {
+    use super::*;
+    /// Query Parameters for [Get Users Follows](super::get_users_follows)
+    ///
+    /// [`get-users-follows`](https://dev.twitch.tv/docs/api/reference#get-users-follows)
+    #[derive(PartialEq, TypedBuilder, Deserialize, Serialize, Clone, Debug)]
+    #[non_exhaustive]
+    pub struct GetUsersFollowsRequest {
+        /// Cursor for forward pagination: tells the server where to start fetching the next set of results, in a multi-page response. The cursor value specified here is from the pagination response field of a prior query.
+        #[builder(default)]
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub after: Option<helix::Cursor>,
+        /// Maximum number of objects to return. Maximum: 100. Default: 20.
+        #[builder(default)]
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub first: Option<usize>,
+        /// User ID. The request returns information about users who are being followed by the from_id user.
+        #[builder(default)]
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub from_id: Option<String>,
+        /// User ID. The request returns information about users who are following the to_id user.
+        #[builder(default)]
+        #[serde(skip_serializing_if = "Option::is_none")]
+        pub to_id: Option<String>,
+    }
+
+    /// Return Values for [Get Users Follows](super::get_users_follows)
+    ///
+    /// [`get-users-follows`](https://dev.twitch.tv/docs/api/reference#get-users-follows)
+    #[derive(PartialEq, Deserialize, Debug, Clone)]
+    #[non_exhaustive]
+    pub struct UsersFollows {
+        ///Date and time when the from_id user followed the to_id user.
+        pub followed_at: String,
+        ///ID of the user following the to_id user.
+        pub from_id: String,
+        ///Display name corresponding to from_id.
+        pub from_name: String,
+        ///ID of the user being followed by the from_id user.
+        pub to_id: String,
+        ///Display name corresponding to to_id.
+        pub to_name: String,
+        // FIXME: This never seems to be returned.
+        /// Total number of items returned.
+        ///
+        /// * If only `from_id` was in the request, this is the total number of followed users.
+        /// * If only `to_id` was in the request, this is the total number of followers.
+        /// * If both `from_id` and to_id were in the request, this is 1 (if the "from" user follows the "to" user) or 0.
+        pub total: Option<usize>,
+    }
+
+    impl helix::Request for GetUsersFollowsRequest {
+        type Response = UsersFollows;
+
+        const OPT_SCOPE: &'static [twitch_oauth2::Scope] = &[];
+        const PATH: &'static str = "users/follows";
+        const SCOPE: &'static [twitch_oauth2::Scope] = &[];
+    }
+
+    impl helix::RequestGet for GetUsersFollowsRequest {}
+    impl helix::Paginated for GetUsersFollowsRequest {
+        fn set_pagination(&mut self, cursor: helix::Cursor) { self.after = Some(cursor); }
+    }
 }
