3af2b98968a2551e6d4dd1c82be1529ea0c4cf2c
Emilgardis <Emilgardis@users.noreply.github.com>
2020-10-15 19:01:37 +0000

    deploy: 6266a4ae6a9b25441a4a36096cedfafb4c83ba0e

--- twitch_api2/helix/users/create_user_follows/sidebar-items.js
+++ twitch_api2/helix/users/create_user_follows/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["CreateUserFollows","Return Values for [Create Users Follows"]],"struct":[["CreateUserFollowsBody","Body Parameters for Create Users Follows"],["CreateUserFollowsRequest","Query Parameters for Create Users Follows"]]});
+initSidebarItems({"enum":[["CreateUserFollows","Return Values for Create User Follows"]],"struct":[["CreateUserFollowsBody","Body Parameters for Create User Follows"],["CreateUserFollowsRequest","Query Parameters for Create User Follows"]]});
