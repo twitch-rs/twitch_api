fb13b9aba6964473037e87f984e58123e9a651a7
Emil Gardström <emil.gardstrom@gmail.com>
2021-02-13 11:58:47 +0100

    update example

--- src/helix/users/get_users.rs
+++ src/helix/users/get_users.rs
@@ -107,20 +107,22 @@ fn test_request() {
     // FIXME: This is not valid anymore. Twitch....
     let data = br#"
 {
-    "data": [{
-        "id": "44322889",
-        "login": "dallas",
-        "display_name": "dallas",
-        "type": "staff",
-        "broadcaster_type": "",
-        "description": "Just a gamer playing games and chatting. :)",
-        "profile_image_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/dallas-profile_image-1a2c906ee2c35f12-300x300.png",
-        "offline_image_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/dallas-channel_offline_image-1a2c906ee2c35f12-1920x1080.png",
-        "view_count": 191836881,
-        "email": "login@provider.com",
-        "created_at": "2013-06-03T19:12:02.580593Z"
-    }]
-}
+    "data": [
+        {
+        "id": "141981764",
+        "login": "twitchdev",
+        "display_name": "TwitchDev",
+        "type": "",
+        "broadcaster_type": "partner",
+        "description": "Supporting third-party developers building Twitch integrations from chatbots to game integrations.",
+        "profile_image_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/8a6381c7-d0c0-4576-b179-38bd5ce1d6af-profile_image-300x300.png",
+        "offline_image_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/3f13ab61-ec78-4fe6-8481-8682cb3b0ac2-channel_offline_image-1920x1080.png",
+        "view_count": 5980557,
+        "email": "not-real@email.com",
+        "created_at": "2016-12-14T20:32:28.894263Z"
+        }
+    ]
+    }
 "#
         .to_vec();
 
