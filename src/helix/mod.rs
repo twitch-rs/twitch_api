3a3e9c46a05fcbf79a2e0bb2b690919eb0d640e7
Emil Gardström <emil.gardstrom@gmail.com>
2021-03-28 03:48:30 +0200

    remove note about overriding
    also adds fixme on request argument on RequestPost::parse_response

--- src/helix/mod.rs
+++ src/helix/mod.rs
@@ -369,8 +369,13 @@ pub trait RequestPost: Request {
             .map_err(Into::into)
     }
 
-    /// Parse response. Override for different behavior
+    /// Parse response.
+    /// 
+    /// # Notes
+    /// 
+    /// Pass in the request to enable [pagination](Response::get_next) if supported.
     fn parse_response(
+        // FIXME: Is this really needed? Its currently only used for error reporting.
         request: Option<Self>,
         uri: &http::Uri,
         response: http::Response<Vec<u8>>,
@@ -442,7 +447,7 @@ where <Self as Request>::Response:
             .map_err(Into::into)
     }
 
-    /// Parse response. Override for different behavior
+    /// Parse response.
     fn parse_response(
         uri: &http::Uri,
         response: http::Response<Vec<u8>>,
@@ -488,7 +493,7 @@ pub trait RequestDelete: Request {
             .map_err(Into::into)
     }
 
-    /// Parse response. Override for different behavior
+    /// Parse response.
     fn parse_response(
         uri: &http::Uri,
         response: http::Response<Vec<u8>>,
@@ -564,7 +569,7 @@ where <Self as Request>::Response:
             .map_err(Into::into)
     }
 
-    /// Parse response. Override for different behavior
+    /// Parse response.
     fn parse_response(
         uri: &http::Uri,
         response: http::Response<Vec<u8>>,
@@ -628,7 +633,11 @@ pub trait RequestGet: Request {
             .map_err(Into::into)
     }
 
-    /// Parse response. Override for different behavior
+    /// Parse response.
+    /// 
+    /// # Notes
+    /// 
+    /// Pass in the request to enable [pagination](Response::get_next) if supported.
     fn parse_response(
         request: Option<Self>,
         uri: &http::Uri,
