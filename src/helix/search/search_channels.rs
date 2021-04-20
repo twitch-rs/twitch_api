119dd24b6adcede311e966fcf4fa4d136ee6dd84
Emil Gardström <emil.gardstrom@gmail.com>
2021-04-21 00:50:37 +0200

    add game_name to Search Channels

--- src/helix/search/search_channels.rs
+++ src/helix/search/search_channels.rs
@@ -69,6 +69,8 @@ pub struct SearchChannelsRequest {
 pub struct Channel {
     /// ID of the game being played on the stream
     pub game_id: types::CategoryId,
+    /// Name of the game being played on the stream.
+    pub game_name: String,
     /// Channel ID
     pub id: types::UserId,
     /// Display name corresponding to user_id
@@ -111,25 +113,26 @@ fn test_request() {
 
     // From twitch docs
     let data = br#"
-{
-    "data": [
-        {
+    {
+        "data": [
+          {
             "broadcaster_language": "en",
             "broadcaster_login": "a_seagull",
             "display_name": "A_Seagull",
             "game_id": "506442",
+            "game_name": "DOOM Eternal",
             "id": "19070311",
             "is_live": true,
             "tags_ids": [
-                "6ea6bca4-4712-4ab9-a906-e3336a9d8039"
+              "6ea6bca4-4712-4ab9-a906-e3336a9d8039"
             ],
             "thumbnail_url": "https://static-cdn.jtvnw.net/jtv_user_pictures/a_seagull-profile_image-4d2d235688c7dc66-300x300.png",
             "title": "a_seagull",
             "started_at": "2020-03-18T17:56:00Z"
-        }
-    ],
-    "pagination": {}
-}
+          }
+        ],
+        "pagination": {}
+      }
 "#
         .to_vec();
 
