ee2e6c0599999d7724ffab0868bbf335fd079410
Emil Gardström <emil.gardstrom@gmail.com>
2022-03-04 21:09:45 +0100

    disable all endpoints except get_users

--- src/helix/endpoints/mod.rs
+++ src/helix/endpoints/mod.rs
@@ -1,26 +1,26 @@
-pub mod bits;
-pub mod channels;
-#[cfg(feature = "unsupported")]
-pub mod charity;
-pub mod chat;
-pub mod clips;
-#[cfg(feature = "eventsub")]
-#[cfg_attr(nightly, doc(cfg(feature = "eventsub")))]
-pub mod eventsub;
-pub mod games;
-pub mod goals;
-pub mod hypetrain;
-pub mod moderation;
-pub mod points;
-pub mod polls;
-pub mod predictions;
-pub mod raids;
-pub mod schedule;
-pub mod search;
-pub mod streams;
-pub mod subscriptions;
-pub mod tags;
-pub mod teams;
+// pub mod bits;
+// pub mod channels;
+// #[cfg(feature = "unsupported")]
+// pub mod charity;
+// pub mod chat;
+// pub mod clips;
+// #[cfg(feature = "eventsub")]
+// #[cfg_attr(nightly, doc(cfg(feature = "eventsub")))]
+// pub mod eventsub;
+// pub mod games;
+// pub mod goals;
+// pub mod hypetrain;
+// pub mod moderation;
+// pub mod points;
+// pub mod polls;
+// pub mod predictions;
+// pub mod raids;
+// pub mod schedule;
+// pub mod search;
+// pub mod streams;
+// pub mod subscriptions;
+// pub mod tags;
+// pub mod teams;
 pub mod users;
-pub mod videos;
-pub mod whispers;
+// pub mod videos;
+// pub mod whispers;
